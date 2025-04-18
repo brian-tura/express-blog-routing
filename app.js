f4f083c1ce0b8eab5d4e65ee75287bd81a083b94
brian-tura <b.tura2002@gmail.com>
2025-04-18 20:17:32 +0200

    [DONE] Excercise done

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const postRouter = require('./routers/posts/')
+const postRouter = require('./routers/posts.js')
 
 app.use(express.static('public'));
 
 app.use("/posts", postRouter);
+
+app.get('/', (req, res) => {
+    res.send('I miei posts')
+})
 
 app.listen(port, () => {
     console.log(`App in ascolto sulla porta ${port}`);
