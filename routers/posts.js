7165e064beb36ee018fca01b7f52f03d11302d09
brian-tura <b.tura2002@gmail.com>
2025-04-18 19:58:25 +0200

    [FEAT] Added all CRUD operations

--- routers/posts.js
+++ routers/posts.js
@@ -10,8 +10,24 @@ app.get('/posts', (req, res) => {
     res.send('Lista dei post');
 });
 
-app.get('/posts:id', (req, res) => {
+app.get('/posts/:id', (req, res) => {
     res.send(`Dettagli del post ${req.params.id}`);
+});
+
+app.post('/posts', (req, res) => {
+    res.send('Creazione nuovo post');
+});
+
+app.put('/posts/:id', (req, res) => {
+    res.send(`Modifica integrale del post ${req.params.id}`);
+});
+
+app.patch('/posts/:id', (req, res) => {
+    res.send(`Modifica parziale del post ${req.params.id}`);
+});
+
+app.delete('/posts/:id', (req, res) => {
+    res.send(`Eliminazione del post ${req.params.id}`);
 });
 
 app.listen(port, () => {
