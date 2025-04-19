9522fcdfb3edb0b30922ab5bb4bab30fae71846e
brian-tura <b.tura2002@gmail.com>
2025-04-19 17:15:24 +0200

    [FIX] Adjusted spacing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const postRouter = require('./routers/posts.js')
- 
+
 app.use(express.static('public'));
 
 app.use("/posts", postRouter);
